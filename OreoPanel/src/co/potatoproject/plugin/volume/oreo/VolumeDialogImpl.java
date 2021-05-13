a6523c3deed2ceae513c924cad67e32e373b2a8a
Pranav Vashi <neobuddy89@gmail.com>
2021-05-13 19:57:17 +0530

    VolumePanel: Add changes for live volume steps
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id841b03701191b50eefa054e31044f13206ee51d

--- OreoPanel/src/co/potatoproject/plugin/volume/oreo/VolumeDialogImpl.java
+++ OreoPanel/src/co/potatoproject/plugin/volume/oreo/VolumeDialogImpl.java
@@ -1021,7 +1021,8 @@ public class VolumeDialogImpl implements VolumeDialog {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -1101,7 +1102,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         final boolean enableSlider = !zenMuted;
         final int vlevel = row.ss.muted && (!isRingStream && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowSliderTintH(VolumeRow row, boolean isActive) {
@@ -1114,7 +1115,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -1138,7 +1139,7 @@ public class VolumeDialogImpl implements VolumeDialog {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
