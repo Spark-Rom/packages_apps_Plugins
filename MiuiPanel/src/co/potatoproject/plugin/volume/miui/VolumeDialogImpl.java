5ffad476bfddfbe689e3891dbbbd57861d6b7dc6
spkal01 <kalligeross@gmail.com>
2021-07-06 00:53:11 +0530

    Revert "MiuiPanel: Fix alarm not showing"
    This reverts commit 8636b59b316fb68d54b18141924fc4edfeee030b.

--- MiuiPanel/src/co/potatoproject/plugin/volume/miui/VolumeDialogImpl.java
+++ MiuiPanel/src/co/potatoproject/plugin/volume/miui/VolumeDialogImpl.java
@@ -512,7 +512,7 @@ public class VolumeDialogImpl implements VolumeDialog {
     private void cleanExpandedRows() {
         for (int i = mRows.size() - 1; i >= 0; i--) {
             final VolumeRow row = mRows.get(i);
-            if (row.stream == AudioManager.STREAM_RING || row.stream == AudioManager.STREAM_ALARM) {
+            if (row.stream == AudioManager.STREAM_RING) {
                 removeRow(row);
             }
         }
@@ -661,8 +661,6 @@ public class VolumeDialogImpl implements VolumeDialog {
                 if (!mExpanded) {
                     addRow(AudioManager.STREAM_RING, mSysUIR.drawable("ic_volume_ringer"),
                             mSysUIR.drawable("ic_volume_ringer_mute"), true, false);
-					addRow(AudioManager.STREAM_ALARM, mSysUIR.drawable("ic_volume_alarm"),
-                            mSysUIR.drawable("ic_alarm"), true, false);
                     updateAllActiveRows();
                     mExpanded = true;
                     updateOutputSwitcherVisibility();
